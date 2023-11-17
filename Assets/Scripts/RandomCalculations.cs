6692e8ae4942491a40611de3524b332fee98be90
bhixsonsimeral16 <brian.hixsonsimeral@gmail.com>
2023-11-16 19:34:19 -0500

    Lesson 2 part 1
    Began work on static methods and inheritance

--- Assets/Scripts/RandomCalculations.cs
+++ Assets/Scripts/RandomCalculations.cs
@@ -7,10 +7,19 @@ public class RandomCalculations : MonoBehaviour
     public float height;
     public float width;
     public float area;
+
+    public static int randNum = 0;
     // Start is called before the first frame update
     void Start()
     {
+        CalculateRandomNumber();
         CalculateArea();
+    }
+
+    public static void CalculateRandomNumber()
+    {
+        randNum++;
+        Debug.Log($"Random number is {randNum}");
     }
 
     // Update is called once per frame
